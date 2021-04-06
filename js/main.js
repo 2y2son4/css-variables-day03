138eb988b1aaf27aa372e203c7f13d3f96e50489
2y2son4 <adrianadelteso@gmail.com>
2021-04-06 23:11:58 +0200

    :tada: Initial commit

---
+++ js/main.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const inputs = document.querySelectorAll('.controls input');
+
+const btn = document.querySelector('.reset');
+
+function handleUpdate() {
+  const suffix = this.dataset.sizing || '';
+  //   console.log(this.value);
+  //     console.log(suffix);
+  //   console.log(this.name);
+  //   console.log(inputs);
+
+  document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+}
+
+inputs.forEach((input) => input.addEventListener('change', handleUpdate));
+inputs.forEach((input) => input.addEventListener('mousemove', handleUpdate));
+
+function handleReset() {
+  location.reload();
+}
+
+btn.addEventListener('click', handleReset);
